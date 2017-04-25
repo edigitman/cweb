3b67a5dff09496471af3ef8a5a644768fcd2fdac
Alexandru Eduard Gitman <alexandru.eduard.gitman@ing.be>
2017-04-25 16:43:35 +0200

    initial import

---
+++ src/main/java/app/book/Book.java
@@ -0,0 +1,47 @@
+package app.book;
+
+
+public class Book {
+    String title;
+    String author;
+    String isbn;
+
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    public String getMediumCover() {
+        return "http://covers.openlibrary.org/b/isbn/" + this.isbn + "-M.jpg";
+    }
+
+    public String getLargeCover() {
+        return "http://covers.openlibrary.org/b/isbn/" + this.isbn + "-L.jpg";
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
